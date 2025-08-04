2a753cd0648e691261b4b240aaa7c72883f08043
yangww <dayang_techmail@163.com>
2025-08-04 17:26:39 +0800

    可视化 表达式语法树

---
+++ LoxSharp/Ast/AstVisualizer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LoxSharp
+{
+    public class AstVisualizer : Expr.Visitor<string>
+    {
+        public string Print(Expr expr)
+        {
+            return expr.Accept(this);
+        }
+
+        private string Indent(string text, string prefix)
+        {
+            var lines = text.Split('\n');
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = prefix + lines[i];
+            }
+            return string.Join("\n", lines);
+        }
+
+        public string VisitBinaryExpr(Expr.Binary expr)
+        {
+            var left = Indent(expr.Left.Accept(this), "├── ");
+            var right = Indent(expr.Right.Accept(this), "└── ");
+            return $"Binary {expr.Operator.Lexeme}\n{left}\n{right}";
+        }
+
+        public string VisitGroupingExpr(Expr.Grouping expr)
+        {
+            var inner = Indent(expr.Expression.Accept(this), "└── ");
+            return $"Grouping\n{inner}";
+        }
+
+        public string VisitLiteralExpr(Expr.Literal expr)
+        {
+            return $"Literal {expr.Value}";
+        }
+
+        public string VisitUnaryExpr(Expr.Unary expr)
+        {
+            var right = Indent(expr.Right.Accept(this), "└── ");
+            return $"Unary {expr.Operator.Lexeme}\n{right}";
+        }
+    }
+}
--- LoxSharp/Program.cs
+++ LoxSharp/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            TestScanner();
+            PrintAst();
             Console.ReadKey();
         }
 
@@ -23,6 +23,18 @@
             {
                 Console.WriteLine(token);
             }
+        }
+
+        static void PrintAst()
+        {
+            Expr expression = new Expr.Binary(
+                new Expr.Literal(1.0),
+                new Token(TokenType.PLUS, "+", null, 1),
+                new Expr.Binary(new Expr.Literal(2.0),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Literal(3.0)));
+            var visualizer = new AstVisualizer();
+            Console.WriteLine(visualizer.Print(expression));
         }
     }
 }
